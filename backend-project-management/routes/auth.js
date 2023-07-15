9bbe10e18e95392c69864a5e25c56f7f6d6a3ec0
omardieh <omar.dieh@wildcodeschool.com>
2023-07-15 02:04:06 +0200

    add backend:i

---
+++ backend-project-management/routes/auth.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+
+const router = express.Router();
+const saltRounds = 10;
